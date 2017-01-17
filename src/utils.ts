8c879000e559d6d9ab698f290db3a1816c92dc4d
mailto1587 <mailto1587@gmail.com>
2017-01-17 13:08:39 +0800

    Add parseColor

--- src/utils.ts
+++ src/utils.ts
@@ -63,3 +63,20 @@ export function captureTouch(element: HTMLElement): ITouch {
 
     return touch;
 }
+
+export function parseColor(color: string | number, toNumber: boolean): string | number {
+    if (toNumber === true) {
+        if (typeof color === 'number') {
+            return (color | 0);
+        }
+        if (typeof color === 'string' && color[0] === '#') {
+            color = color.slice(1);
+        }
+        return parseInt(color, 16);
+    } else {
+        if (typeof color === 'number') {
+            color = '#' + ('00000' + (color | 0).toString(16)).substr(-6);
+        }
+        return color;
+    }
+}
